bc1353260129bd65b5d5d4fb844c1b4680bee2e4
Helder Rodrigues Mendes <helderrodrigues37@gmail.com>
2020-09-08 19:42:47 -0300

    Configurando tela Home

--- app/src/main/java/com/example/controlealimentos/app/view/HomeActivity.java
+++ app/src/main/java/com/example/controlealimentos/app/view/HomeActivity.java
@@ -1,16 +1,43 @@
 package com.example.controlealimentos.app.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.controlealimentos.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView cadCompra, cadProduto, consulCompra, consulProduto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        cadCompra = findViewById(R.id.cad_compra);
+        cadProduto = findViewById(R.id.cad_produto);
+        consulCompra = findViewById(R.id.consul_compra);
+        consulProduto = findViewById(R.id.consul_produto);
+
+        cadCompra.setOnClickListener(this);
+        cadProduto.setOnClickListener(this);
+        consulCompra.setOnClickListener(this);
+        consulProduto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+            case R.id.consul_produto:i=new Intent(this, List_ProdutosActivity.class);
+            startActivity(i);
+            break;
+        }
+
     }
 }
